3cc9c78a8658b272045d64897afad719aadd047e
yoonho <qufdbsgh431@gmail.com>
2022-08-23 16:36:22 +0900

    메인 페이지 템플릿 구성

--- mytodo/todo/views.py
+++ mytodo/todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.filter(complete=False)
+    return render(request, 'todo/todo_list.html',{'todos':todos}) # {템플릿 변수이름: 파이썬 변수이름}
